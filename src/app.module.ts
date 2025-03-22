b15c8ca5611a8cc1f988839eab764cd0a3562bbb
Mohammad Amanullah <ullahaman072003@gmail.com>
2025-03-22 23:04:00 +0530

    created app modules

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { RolesModule } from './roles/roles.module';
+
+@Module({
+  imports: [DatabaseModule, AuthModule, UsersModule, RolesModule],
+})
+export class AppModule {}
